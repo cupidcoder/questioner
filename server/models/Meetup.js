af5c5deb72100280b8672724b337498d099fdb7e
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2019-01-08 12:14:06 +0100

    [chore #163023304] Add seeder for meetup model

--- server/models/Meetup.js
+++ server/models/Meetup.js
@@ -1,3 +1,4 @@
+import uuid from 'uuid/v4';
 /**
  * Meetup model that holds the Schema for meetups
  * @package Meetup
@@ -6,7 +7,37 @@
 class Meetup {
   constructor() {
     this.meetups = [];
+    this.seeder();
     return this.meetups;
+  }
+
+  /**
+   * Seed dummy meetups data into this.meetups
+   */
+
+  seeder() {
+    // Dummy data
+    const ids = [uuid(), uuid(), uuid(), uuid()];
+    const date = new Date();
+    const createdOn = [date.getTime(), date.getTime() + 1, date.getTime() + 2, date.getTime() + 3];
+    const meetupLocations = ['Ikeja', 'Mushin', 'Gbagada', 'Ogba'];
+    const meetupTopics = ['Nodejs', 'Laravel', 'Golang', 'PyConf'];
+    const meetupDescriptions = ['First description', 'Second description', 'Third description', 'Fourth description'];
+    const happeningOn = [date.getTime() + 1000, date.getTime() + 432, date.getTime() + 788, date.getTime() + 23];
+    // Seed dummy data
+    for (let i = 0; i < 4; i += 1) {
+      const meetupRecord = {
+        id: ids[i],
+        createdOn: createdOn[i],
+        location: meetupLocations[i],
+        images: '',
+        topic: meetupTopics[i],
+        description: meetupDescriptions[i],
+        happeningOn: happeningOn[i],
+        tags: '',
+      };
+      this.meetups.push(meetupRecord);
+    }
   }
 }
 
--- server/models/Question.js
+++ server/models/Question.js
@@ -1,3 +1,4 @@
+import uuid from 'uuid/v4';
 /**
  * Question model that holds the Schema for questions
  * @package Question
@@ -6,7 +7,35 @@
 class Question {
   constructor() {
     this.questions = [];
+    this.seeder();
     return this.questions;
+  }
+
+  /**
+   * Seed dummy questions data into this.questions
+   */
+  seeder() {
+    // Dummy data
+    const ids = [uuid(), uuid(), uuid(), uuid()];
+    const date = new Date();
+    const createdOn = [date.getTime(), date.getTime() + 1, date.getTime() + 2, date.getTime() + 3];
+    const createdBy = [uuid(), uuid(), uuid(), uuid()];
+    const meetupIDs = [uuid(), uuid(), uuid(), uuid()];
+    const questionTitles = ['First title', 'Second title', 'Third title', 'Fourth title'];
+    const questionBody = ['First body', 'Second body', 'Third body', 'Fourth body'];
+    // Seed dummy data
+    for (let i = 0; i < 4; i += 1) {
+      const questionRecord = {
+        id: ids[i],
+        createdOn: createdOn[i],
+        createdBy: createdBy[i],
+        meetup: meetupIDs[i],
+        title: questionTitles[i],
+        body: questionBody[i],
+        votes: 0,
+      };
+      this.questions.push(questionRecord);
+    }
   }
 }
 
