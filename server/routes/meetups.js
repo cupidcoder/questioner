be817f91caf82387025af6868d03d9a199eb376b
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2019-01-11 17:19:15 +0100

    [Chore #163023304] Create middleware validation helper function

--- server/routes/meetups.js
+++ server/routes/meetups.js
@@ -1,12 +1,13 @@
 import express from 'express';
+import validation from '../middlewares/validation';
 import MeetupController from '../controllers/Meetup';
 
 const router = express.Router();
 
 router.get('/', MeetupController.getAll);
 router.get('/upcoming/', MeetupController.getUpcoming);
-router.post('/', MeetupController.create);
+router.post('/', validation.validateMeetup, MeetupController.create);
 router.get('/:id', MeetupController.getOne);
-router.post('/:id/rsvp', MeetupController.respondToMeetup);
+router.post('/:id/rsvp', validation.validateRSVP, MeetupController.respondToMeetup);
 
 module.exports = router;
